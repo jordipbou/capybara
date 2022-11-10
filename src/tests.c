2ff02dc82fb0126756e7be4199cf55fed134d733
jordipbou <jordipbou@protonmail.com>
2022-11-10 11:34:20 +0100

    Version 1.0. As simple as I can do it but with enough functionality to create a machine code Forth language.

---
+++ src/tests.c
@@ -0,0 +1,224 @@
+#include <stdlib.h>
+
+#include "unity.h"
+#include "vm.h"
+
+void setUp(void) {}
+
+void tearDown(void) {}
+
+void test_block_initialization(void) {
+	CELL data_size = 1024;
+	CELL code_size = 1024;
+
+	CTX* ctx = init(data_size, code_size);
+	TEST_ASSERT_NOT_NULL(ctx);
+
+	TEST_ASSERT_GREATER_OR_EQUAL_INT(data_size, ctx->dsize);
+	TEST_ASSERT_GREATER_OR_EQUAL_INT(code_size, ctx->csize);
+
+	TEST_ASSERT_EQUAL_INT(0, ctx->Fx);
+	TEST_ASSERT_EQUAL_INT(0, ctx->Lx);
+
+	TEST_ASSERT_NOT_NULL(ctx->code);
+
+	TEST_ASSERT_EQUAL_PTR(ctx, ALIGN(ctx, pagesize()));
+	TEST_ASSERT_EQUAL_PTR(ctx->code, ALIGN(ctx->code, pagesize()));
+
+	TEST_ASSERT_EQUAL_PTR(ctx->dhere, ALIGN(ctx->dhere, 2*sizeof(CELL)));
+	
+	deinit(ctx);
+}
+
+void test_compile_byte(void) {
+	CTX* ctx = init(1024, 1024);
+
+	TEST_ASSERT_EQUAL_INT(ctx->code, ctx->chere);
+
+	BYTE* chere = unprotect(ctx);
+	compile_lit(ctx, BYTE, 0xC3);
+	protect(ctx, chere);
+
+	TEST_ASSERT_EQUAL_INT(ctx->code + 1, ctx->chere);
+
+	TEST_ASSERT_EQUAL_INT8(0xC3, *(ctx->code));
+
+	chere = unprotect(ctx);
+	compile_lit(ctx, BYTE, 0xB8);
+	protect(ctx, chere);
+
+	TEST_ASSERT_EQUAL_INT(ctx->code + 2, ctx->chere);
+
+	TEST_ASSERT_EQUAL_INT8(0xC3, *(ctx->code));
+	TEST_ASSERT_EQUAL_INT8(0xB8, *(ctx->code + 1));
+
+	deinit(ctx);
+}
+
+void test_compile_bytes(void) {
+	CTX* ctx = init(1024, 1024);
+
+	TEST_ASSERT_EQUAL_INT(ctx->code, ctx->chere);
+
+	BYTE* chere = unprotect(ctx);
+	compile_bytes(ctx, "\xB8\x11\xFF\x00\xFF", 5);
+	protect(ctx, chere);
+
+	TEST_ASSERT_EQUAL_INT(ctx->code + 5, ctx->chere);
+
+	TEST_ASSERT_EQUAL_INT8(0xB8, *(ctx->code));
+	TEST_ASSERT_EQUAL_INT8(0x11, *(ctx->code + 1));
+	TEST_ASSERT_EQUAL_INT8(0xFF, *(ctx->code + 2));
+	TEST_ASSERT_EQUAL_INT8(0x00, *(ctx->code + 3));
+	TEST_ASSERT_EQUAL_INT8(0xFF, *(ctx->code + 4));
+
+	unprotect(ctx);
+	compile_bytes(ctx, "\xC7\x47", 2);
+	protect(ctx, chere);
+
+	TEST_ASSERT_EQUAL_INT(ctx->code + 7, ctx->chere);
+
+	TEST_ASSERT_EQUAL_INT8(0xC7, *(ctx->code + 5));
+	TEST_ASSERT_EQUAL_INT8(0x47, *(ctx->code + 6));
+
+	deinit(ctx);
+}
+
+void test_compile_literal(void) {
+	CTX* ctx = init(1024, 1024);
+
+	TEST_ASSERT_EQUAL_INT(ctx->code, ctx->chere);
+
+	BYTE* chere = unprotect(ctx);
+	compile_lit(ctx, CELL, 41378);
+	protect(ctx, chere);
+
+	TEST_ASSERT_EQUAL_INT(ctx->code + sizeof(CELL), ctx->chere);
+
+	TEST_ASSERT_EQUAL_INT64(41378, *((CELL*)(ctx->code)));
+
+	chere = unprotect(ctx);
+	compile_lit(ctx, CELL, 13);
+	protect(ctx, chere);
+
+	TEST_ASSERT_EQUAL_INT(ctx->code + 2*sizeof(CELL), ctx->chere);
+
+	TEST_ASSERT_EQUAL_INT64(13, *((CELL*)(ctx->code + sizeof(CELL))));
+
+	chere = unprotect(ctx);
+	compile_lit(ctx, HALF, 19923);
+	protect(ctx, chere);
+
+	TEST_ASSERT_EQUAL_INT(ctx->code + 2*sizeof(CELL) + sizeof(HALF), ctx->chere);
+
+	TEST_ASSERT_EQUAL_INT32(19923, *((HALF*)(ctx->code + 2*sizeof(CELL))));
+
+	chere = unprotect(ctx);
+	compile_lit(ctx, HALF, -367);
+	protect(ctx, chere);
+
+	TEST_ASSERT_EQUAL_INT(ctx->code + 2*sizeof(CELL) + 2*sizeof(HALF), ctx->chere);
+
+	TEST_ASSERT_EQUAL_INT32(-367, *((HALF*)(ctx->code + 2*sizeof(CELL) + sizeof(HALF))));
+
+	deinit(ctx);
+}
+
+void test_compile_next(void) {
+	CTX* ctx = init(1024, 1024);
+	
+	TEST_ASSERT_EQUAL_INT(ctx->code, ctx->chere);
+
+	BYTE* chere = unprotect(ctx);
+	compile_next(ctx);
+	protect(ctx, chere);
+
+	TEST_ASSERT_EQUAL_INT(ctx->code + 8, ctx->chere);
+
+	BYTE* NEXT = CALL(ctx->code, ctx);
+
+	TEST_ASSERT_EQUAL_PTR(ctx->code + 8, NEXT);
+
+	chere = unprotect(ctx);
+	compile_next(ctx);
+	protect(ctx, chere);
+
+	TEST_ASSERT_EQUAL_INT(ctx->code + 16, ctx->chere);
+
+	NEXT = CALL(NEXT, ctx);
+
+	TEST_ASSERT_EQUAL_PTR(ctx->code + 16, NEXT);
+
+	deinit(ctx);
+}
+
+void generic_cfunc(CTX* ctx) {
+}
+
+void test_compile_cfunc(void) {
+	CTX* ctx = init(1024, 1024);
+
+	TEST_ASSERT_EQUAL_INT(ctx->code, ctx->chere);
+
+	BYTE* chere = unprotect(ctx);
+	compile_cfunc(ctx, &generic_cfunc);
+	compile_next(ctx);
+	protect(ctx, chere);
+
+	TEST_ASSERT_EQUAL_INT(ctx->code + 22, ctx->chere);
+
+	BYTE* NEXT = CALL(ctx->code, ctx);
+
+	TEST_ASSERT_EQUAL_PTR(&generic_cfunc, ctx->Fx);
+	TEST_ASSERT_EQUAL_INT(ctx->code + 22, NEXT);
+
+	deinit(ctx);
+}
+
+void test_compile_push(void) {
+	CTX* ctx = init(1024, 1024);
+
+	TEST_ASSERT_EQUAL_INT(ctx->code, ctx->chere);
+
+	TEST_ASSERT_EQUAL_INT(0, ctx->Lx);
+
+	BYTE* chere = unprotect(ctx);
+	compile_push(ctx, 13);
+	compile_next(ctx);
+	protect(ctx, chere);
+
+	TEST_ASSERT_EQUAL_INT(ctx->code + 22, ctx->chere);
+
+	BYTE* NEXT = CALL(ctx->code, ctx);
+
+	TEST_ASSERT_EQUAL_INT(13, ctx->Lx);
+	TEST_ASSERT_EQUAL_INT(ctx->code + 22, NEXT);
+
+	chere = unprotect(ctx);
+	compile_push(ctx, 17);
+	compile_next(ctx);
+	protect(ctx, chere);
+
+	NEXT = CALL(NEXT, ctx);
+
+	TEST_ASSERT_EQUAL_INT(17, ctx->Lx);
+	TEST_ASSERT_EQUAL_INT(ctx->code + 44, NEXT);
+
+	deinit(ctx);
+}
+
+int main(void) {
+	UNITY_BEGIN();
+
+	RUN_TEST(test_block_initialization);
+
+	RUN_TEST(test_compile_byte);
+	RUN_TEST(test_compile_bytes);
+	RUN_TEST(test_compile_literal);
+
+	RUN_TEST(test_compile_next);
+	RUN_TEST(test_compile_cfunc);
+	RUN_TEST(test_compile_push);
+
+	return UNITY_END();
+}
---
+++ src/vm.h
@@ -0,0 +1,192 @@
+#include<stdlib.h>
+#include<string.h>
+#include<stddef.h>
+
+#ifdef __linux__
+#include<sys/mman.h>
+#include<unistd.h>
+#elif _WIN32
+#include<windows.h>
+#include<memoryapi.h>
+#include<stdint.h>
+#endif
+
+// DATATYPES ------------------------------------------------------------------
+
+#ifdef __linux__
+typedef int8_t BYTE;
+#endif
+typedef int32_t HALF;
+typedef int64_t CELL;
+
+typedef struct _CTX CTX;
+typedef void (*FUNC)(CTX*);
+
+typedef struct _CTX {
+	CELL dsize, csize;	// Data space size and code space size
+	BYTE* dhere;				// Pointer to start of free memory on data segment
+	BYTE* chere;				// Pointer to start of free memory on code segment
+	FUNC* Fx;						// Virtual register with address of function to call from C 
+	CELL Lx;						// Virtual register for passing literal values to C
+	BYTE* code;					// Pointer to code space
+} CTX;
+
+// CONTEXT CREATION AND DESTRUCTION -------------------------------------------
+
+#define ALIGN(addr, bound)	((CELL)addr + ((CELL)bound-1)) & ~((CELL)bound-1)
+
+CELL pagesize() {
+#if __linux__
+	CELL PAGESIZE = sysconf(_SC_PAGESIZE);
+#elif _WIN32
+	SYSTEM_INFO si;
+	GetSystemInfo(&si);
+	CELL PAGESIZE = si.dwPageSize;
+#endif
+	return PAGESIZE;
+}
+
+// Initializes two memory blocks, one as read-write data space and another
+// one as read-exec for code space.
+// Returns a pointer to a context structure with access to both blocks.
+// Both blocks will be pagesize aligned.
+// Free data space pointer will be aligned to first 2*sizeof(CELL) address
+// above context address + sizeof(CTX);
+
+CTX* init(CELL dsize, CELL csize) {
+	CELL PAGESIZE = pagesize();
+
+	dsize = ALIGN(dsize, PAGESIZE);
+	csize = ALIGN(csize, PAGESIZE);
+
+#if __linux__
+	CTX* ctx = mmap(NULL, dsize, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
+	if (ctx == (void*)-1) return NULL;
+#elif _WIN32
+	CTX *ctx = VirtualAlloc(NULL, dsize, MEM_COMMIT, PAGE_READWRITE);
+	if (!ctx) return NULL;
+#endif
+
+	ctx->dsize = dsize;
+	ctx->csize = csize;
+	ctx->Fx = NULL;
+	ctx->Lx = 0;
+
+#if __linux__
+	ctx->code = mmap(NULL, csize, PROT_READ|PROT_EXEC, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
+	if (ctx->code == (void*)-1) {
+		free(ctx);
+		return NULL;
+	}
+#elif _WIN32
+	ctx->code = VirtualAlloc(NULL, csize, MEM_COMMIT, PAGE_EXECUTE_READ);
+	if (!ctx->code) {
+		free(ctx);
+		return NULL;
+	}
+#endif
+
+	// Start of free memory on data space is cell aligned
+	ctx->dhere = (BYTE*)((CELL)ctx + ALIGN(sizeof(CTX), 2*sizeof(CELL)));
+	ctx->chere = ctx->code;
+
+	return ctx;
+}
+
+void deinit(CTX* ctx) {
+#if __linux__
+	munmap(ctx->code, ctx->csize);
+	munmap(ctx, ctx->dsize);
+#elif _WIN32
+	VirtualFree(ctx->code, 0, MEM_RELEASE);
+	VirtualFree(ctx, 0, MEM_RELEASE);
+#endif
+}
+
+// COMPILATION ----------------------------------------------------------------
+
+// Sets code space protection to WRITE to allow compilation.
+
+BYTE* unprotect(CTX* ctx) {
+#if __linux__
+	if (!mprotect(ctx->code, ctx->csize, PROT_WRITE)) 
+		return ctx->chere;
+	else 
+		return NULL; 
+#elif _WIN32
+	DWORD oldprot;
+	if (VirtualProtect(ctx->code, ctx->csize, PAGE_READWRITE, &oldprot)) 
+		return ctx->chere;
+	else 
+		return NULL;
+#endif
+}
+
+// Sets code space protection back to READ_EXEC for execution.
+
+BYTE* protect(CTX* ctx, BYTE* old_chere) {
+#if __linux__
+	if (!mprotect(ctx->code, ctx->csize, PROT_READ|PROT_EXEC)) 
+		return ctx->chere;
+	else {
+		ctx->chere = old_chere;
+		return NULL;
+	}
+#elif _WIN32
+	DWORD oldprot;
+	if (VirtualProtect(ctx->code, ctx->csize, PAGE_EXECUTE_READ, &oldprot)) 
+		return ctx->chere;
+	else {
+		ctx->chere = old_chere;
+		return NULL;
+	}
+#endif
+}
+
+// Compile helpers
+
+#define compile_lit(ctx, type, lit)\
+	{ *((type*)(ctx->chere)) = ((type)(lit)); ctx->chere += sizeof(type); }
+
+void compile_bytes(CTX* ctx, BYTE* bytes, CELL len) {
+	memcpy(ctx->chere, bytes, len);
+	ctx->chere += len;
+}
+
+void compile_next(CTX* ctx) {
+	// 0:  48 8d 81 <H:Address after ret>	lea    rax,[rcx + <address after ret>]
+	// 7:  c3															ret
+	// 8 bytes
+	compile_bytes(ctx, "\x48\x8D\x81", 3);
+	compile_lit(ctx, HALF, ctx->chere - ctx->code + 5);
+	compile_lit(ctx, BYTE, 0xC3);
+}
+
+void compile_reg(CTX* ctx, CELL lit, BYTE offset) {
+	// 0:  49 ba <C:Address of cfunc>			movabs r10,0xff00ff11ff22ff33
+	// a:  4c 89 52 <B:Fx offset>					mov    Q_WORD PTR [rdx+<Fx offset>],r10
+	// 14 bytes
+	compile_bytes(ctx, "\x49\xBA", 2);
+	compile_lit(ctx, CELL, lit);
+	compile_bytes(ctx, "\x4C\x89\x52", 3);
+	compile_lit(ctx, BYTE, offset);
+}
+
+#define compile_cfunc(ctx, cfunc)	compile_reg(ctx, (CELL)cfunc, offsetof(CTX, Fx))
+#define compile_push(ctx, lit)		compile_reg(ctx, lit, offsetof(CTX, Lx))
+
+// RDI: Not used as windows only accepts 4 parameters on registers
+// RSI: Not used as windows only accepts 4 parameters on registers
+// RDX: Pointer to context
+// RCX: Pointer to code space
+// R8: Reserved, but not used
+// R9: Reserved, but not used
+#ifdef __linux__
+#define CALL(f, ctx)\
+	((BYTE* (*)(void*, void*, CTX*, BYTE*, void*, void*))(f))\
+		(NULL, NULL, ctx, ctx->code, NULL, NULL)
+#elif _WIN32
+#define CALL(f, ctx)\
+	((BYTE* (*)(BYTE*, CTX*, void*, void*))(f))\
+		(ctx->code, ctx, NULL, NULL)
+#endif
